9a0cb62ad93ad7d04a870a1ee6781805f58d4d31
Hasib Turjo <159713835+Bedouin121@users.noreply.github.com>
2026-02-13 10:46:37 +0600

    Support for Render

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
   server: {
     host: "::",
     port: 8080,
@@ -12,7 +16,15 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  preview: {
+    allowedHosts: [
+      "portfolio-website-3yrd.onrender.com",
+      // optional but recommended:
+      // ".onrender.com"
+    ],
+  },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
